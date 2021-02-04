4a47954809cdd5fbd5c059a1085082a3a77cf546
Nick Partridge <nkpart@gmail.com>
2021-02-04 20:50:31 +1000

    Formatting

--- UI.hs
+++ UI.hs
@@ -1,77 +1,128 @@
-{-# language OverloadedStrings, OverloadedLists, TupleSections #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+
 module UI where
 
 import Brick
-    (clickable, AttrName, str, handleEventLensed, (<+>), (<=>), customMain,  attrMap,
-      continue,
-      halt,
-      txt,
-      App(App, appDraw, appChooseCursor, appHandleEvent, appStartEvent,
-          appAttrMap),
-      EventM,
-      Widget,
-      BrickEvent(MouseUp, MouseDown, AppEvent, VtyEvent), Next )
-import Graphics.Vty
-    (Button(BLeft), Output(supportsMode, setMode), Mode(Mouse), defaultConfig, mkVty, outputIface,   Event(EvKey), Key(KChar), Modifier(MCtrl) )
-import Sound.ProteaAudio
-    ( finishAudio, initAudio, loaderAvailable, sampleFromFile ) 
-import Data.IORef (writeIORef, newIORef)
-import Brick.BChan ( newBChan, writeBChan )
-import Brick.Widgets.List (renderListWithIndex, listSelectedAttr, handleListEvent, renderList, list)
-import Q (Q(Sometimes, Always))
-import Paths_metronome_cli ( getDataFileName )
-import Text.Printf ( printf )
+  ( App
+      ( App,
+        appAttrMap,
+        appChooseCursor,
+        appDraw,
+        appHandleEvent,
+        appStartEvent
+      ),
+    AttrName,
+    BrickEvent (AppEvent, MouseUp, VtyEvent),
+    EventM,
+    Next,
+    Widget,
+    attrMap,
+    clickable,
+    continue,
+    customMain,
+    halt,
+    handleEventLensed,
+    str,
+    txt,
+    (<+>),
+    (<=>),
+  )
+
+import Brick.BChan (newBChan, writeBChan)
+import Brick.Util (bg)
 import Brick.Widgets.Border (border)
-import Control.Monad.IO.Class (MonadIO(liftIO))
-import Metronome
-import Control.Lens (view)
-import MyLib (startMetronome)
-import Graphics.Vty.Attributes
-import Brick.Util
-import Data.Maybe (fromJust)
-import Data.Char (digitToInt)
 import Brick.Widgets.Center (hCenter)
+import Brick.Widgets.List (handleListEvent, list, listSelectedAttr, renderListWithIndex)
+import Control.Lens (view)
 import Control.Monad (when)
 
-newtype AppEvent = 
-  Beep Int
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Char (digitToInt)
+import Data.IORef (newIORef, writeIORef)
+import Data.Maybe (fromJust)
+import Graphics.Vty
+  ( Button (BLeft),
+    Event (EvKey),
+    Key (KChar),
+    Mode (Mouse),
+    Modifier (MCtrl),
+    Output (setMode, supportsMode),
+    defaultConfig,
+    mkVty,
+    outputIface,
+  )
+import Graphics.Vty.Attributes (Attr, blue, defAttr)
+import Metronome
+  ( BeatSound (Accent),
+    Metronome (Metronome),
+    addBeat,
+    beat,
+    changeProbSelected,
+    metronomeBeats,
+    metronomeBpm,
+    modifyBpm,
+    removeBeat,
+    setAccent,
+    setPlayed,
+    toggleAccentOnSelected,
+  )
+import MyLib (startMetronome)
+import Paths_metronome_cli (getDataFileName)
+import Q (Q (Always, Sometimes))
+import Sound.ProteaAudio
+  ( finishAudio,
+    initAudio,
+    loaderAvailable,
+    sampleFromFile,
+  )
+import Text.Printf (printf)
+
+newtype AppEvent
+  = Beep Int
 
 clickTrackFile :: IO FilePath
 clickTrackFile = getDataFileName "157-click1.wav"
 
 loadDigits :: IO [(Int, String)]
 loadDigits =
-  let loadDigit = \i -> fmap (i, ) . readFile =<< getDataFileName ("digits/" <> show i)
-   in traverse loadDigit [0..9]
+  let loadDigit = \i -> fmap (i,) . readFile =<< getDataFileName ("digits/" <> show i)
+   in traverse loadDigit [0 .. 9]
 
-data Name = 
-      MinusBox Int | PlusBox Int | Beat Int | U deriving (Eq, Show, Ord)
+data Name
+  = MinusBox Int
+  | PlusBox Int
+  | Beat Int
+  | U
+  deriving (Eq, Show, Ord)
 
 uiMain :: IO ()
 uiMain = do
   let initialState = Metronome 114 (list U [(Always Accent, False), (beat, False), (beat, False), (beat, False)] 10)
-  rr <- newIORef initialState 
+  rr <- newIORef initialState
 
   digits <- loadDigits
 
-  let app = App {
-              appDraw = drawUI digits,
-              appChooseCursor = \_s _locs -> Nothing,
-              appHandleEvent = \s' e -> do
-                  s'' <-
-                    case e of 
-                      (VtyEvent e') -> handleEventLensed s' metronomeBeats handleListEvent e'
-                      _ -> pure s'
-                  let (f, s) = handleEvent s'' e
-                  liftIO (writeIORef rr s)
-                  f s,
-              appStartEvent = pure,
-              appAttrMap = \_s -> attrMap defAttr styles
-             }
+  let app =
+        App
+          { appDraw = drawUI digits,
+            appChooseCursor = \_s _locs -> Nothing,
+            appHandleEvent = \s' e -> do
+              s'' <-
+                case e of
+                  (VtyEvent e') -> handleEventLensed s' metronomeBeats handleListEvent e'
+                  _ -> pure s'
+              let (f, s) = handleEvent s'' e
+              liftIO (writeIORef rr s)
+              f s,
+            appStartEvent = pure,
+            appAttrMap = \_s -> attrMap defAttr styles
+          }
   True <- initAudio 100 48000 512
   True <- loaderAvailable "wav"
 
-  clickTrack <- flip sampleFromFile 1.0 =<< clickTrackFile 
+  clickTrack <- flip sampleFromFile 1.0 =<< clickTrackFile
   eventChan <- Brick.BChan.newBChan 10
   _stop <- startMetronome clickTrack rr (writeBChan eventChan . Beep)
   let buildVty = mkVty defaultConfig
@@ -80,8 +131,13 @@ uiMain = do
   when (supportsMode output Mouse) $ do
     setMode output Mouse True
 
-  finalState <- customMain initialVty buildVty
-                    (Just eventChan) app initialState
+  finalState <-
+    customMain
+      initialVty
+      buildVty
+      (Just eventChan)
+      app
+      initialState
   finishAudio
   print finalState
 
@@ -90,73 +146,69 @@ a ~> b = (a, b)
 
 handleEvent :: Metronome n1 -> BrickEvent Name AppEvent -> (s -> EventM n2 (Next s), Metronome n1)
 handleEvent s e = case e of
-   MouseUp (MinusBox n) (Just BLeft) _ -> continue ~> modifyBpm (\x -> x - n) s
-   MouseUp (PlusBox n) (Just BLeft) _ -> continue ~> modifyBpm (+ n) s
-   MouseUp (UI.Beat n) (Just BLeft) _ -> continue ~> setAccent n s
-   VtyEvent e2 -> case e2 of
-
-      -- Quit
-      EvKey k [m] | k == KChar 'c' && m == MCtrl -> halt ~> s
-      EvKey k _ | k == KChar 'q' -> halt ~> s
-      -- Metronome modifications 
-      EvKey k _ -> continue ~>
-        case lookup k actions of
+  MouseUp (MinusBox n) (Just BLeft) _ -> continue ~> modifyBpm (\x -> x - n) s
+  MouseUp (PlusBox n) (Just BLeft) _ -> continue ~> modifyBpm (+ n) s
+  MouseUp (UI.Beat n) (Just BLeft) _ -> continue ~> setAccent n s
+  VtyEvent e2 -> case e2 of
+    -- Quit
+    EvKey k [m] | k == KChar 'c' && m == MCtrl -> halt ~> s
+    EvKey k _ | k == KChar 'q' -> halt ~> s
+    -- Metronome modifications
+    EvKey k _ ->
+      continue
+        ~> case lookup k actions of
           Nothing -> s
           Just f -> f s
-      _ -> continue ~> s
-   AppEvent (Beep n) ->
-     continue ~> setPlayed n s
-   _ -> continue ~> s
+    _ -> continue ~> s
+  AppEvent (Beep n) ->
+    continue ~> setPlayed n s
+  _ -> continue ~> s
 
 actions :: [(Key, Metronome n -> Metronome n)]
-actions = 
-  [
-    -- Change bpm
-    KChar '.' ~> modifyBpm succ
-  , KChar ',' ~> modifyBpm pred
-  , KChar '>' ~> modifyBpm (\x -> x + 5)
-  , KChar '<' ~> modifyBpm (\x -> x - 5)
-  , KChar '=' ~> addBeat
-  , KChar '-' ~> removeBeat
-  , KChar 'x' ~> toggleAccentOnSelected
-  , KChar '[' ~> changeProbSelected (-0.1)
-  , KChar ']' ~> changeProbSelected 0.1
+actions =
+  [ -- Change bpm
+    KChar '.' ~> modifyBpm succ,
+    KChar ',' ~> modifyBpm pred,
+    KChar '>' ~> modifyBpm (\x -> x + 5),
+    KChar '<' ~> modifyBpm (\x -> x - 5),
+    KChar '=' ~> addBeat,
+    KChar '-' ~> removeBeat,
+    KChar 'x' ~> toggleAccentOnSelected,
+    KChar '[' ~> changeProbSelected (-0.1),
+    KChar ']' ~> changeProbSelected 0.1
   ]
 
 drawUI :: [(Int, String)] -> Metronome Name -> [Widget Name]
-drawUI digits s = 
-  [ 
-        border (hCenter $ displayBpm digits (view metronomeBpm s))
-    <=> (
-       border (clickable (MinusBox 5) $ hCenter $ str " -5 ") 
-       <+> 
-       border (clickable (MinusBox 1) $ hCenter $ str " - ")
-       <+> 
-       border (clickable (PlusBox 1) $ hCenter $ str " + ")
-       <+> 
-       border (clickable (PlusBox 5) $ hCenter $ str " +5 ")
-        )
-    <=> border (renderListWithIndex renderItem True (view metronomeBeats s))
+drawUI digits s =
+  [ border (hCenter $ displayBpm digits (view metronomeBpm s))
+      <=> ( border (clickable (MinusBox 5) $ hCenter $ str " -5 ")
+              <+> border (clickable (MinusBox 1) $ hCenter $ str " - ")
+              <+> border (clickable (PlusBox 1) $ hCenter $ str " + ")
+              <+> border (clickable (PlusBox 5) $ hCenter $ str " +5 ")
+          )
+      <=> border (renderListWithIndex renderItem True (view metronomeBeats s))
   ]
-    where renderItem idx _s (b, thisClick) = clickable (UI.Beat idx) $ hCenter $
-            str " " <=>
-            -- ((if thisClick then txt "> " else txt "  ") <+> padRight Max (str (showBeat b))) <=>
-            ((if thisClick then txt "> " else txt "") <+> str (showBeat b) <+> (if thisClick then txt " <" else txt "")) <=>
-            str " " 
-          showBeat (Always b) = show b
-          showBeat (Sometimes f b) = show b <> " ?? " <> printf "%2.2f" f
+  where
+    renderItem idx _s (b, thisClick) =
+      clickable (UI.Beat idx) $
+        hCenter $
+          str " "
+            <=> ((if thisClick then txt "> " else txt "") <+> str (showBeat b) <+> (if thisClick then txt " <" else txt ""))
+            <=> str " "
+    showBeat (Always b) = show b
+    showBeat (Sometimes f b) = show b <> " ?? " <> printf "%2.2f" f
 
 displayBpm :: [(Int, String)] -> Int -> Widget n
 displayBpm digits num =
-  let thisDigits = fromJust . flip lookup digits <$> digs num
+  let thisDigits = fromJust . flip lookup digits <$> digitsFor num
    in foldl1 (<+>) $ fmap str thisDigits
 
-digs :: Int -> [Int]
-digs = map digitToInt . show
+digitsFor :: Int -> [Int]
+digitsFor = map digitToInt . show
 
 -- Styles
 
 styles :: [(AttrName, Attr)]
-styles = [ 
-    (listSelectedAttr, bg blue)
+styles =
+  [ (listSelectedAttr, bg blue)
   ]
